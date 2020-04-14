fe6075efb9a1a5df2696be1f46f85b1ad0789c7a
Addison Hill <adddison.hill@gmail.com>
2020-04-14 15:32:31 -0500

    added graph and util to ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,13 @@
+from graph import Graph
+from util import Stack
+
+# Transfer to graph terminology
+# Build your graph
+# Traverse your graph
+
+# Since the graph is unidirectional you can flip the edges by flipping the values in the tuple
+# Then we can use BFS to find shortest path to ancestor
+
 
 def earliest_ancestor(ancestors, starting_node):
     pass
